3c31ffc570b97d3b6e0702b0a6c7cfc758fc7554
Jannik Feuerhahn <jannik@feuer.dev>
2020-10-20 02:15:40 +0800

    target -> iOS 10 removal of SceneDelegate Setup of initial VC using code

--- fishapp/ViewController.swift
+++ fishapp/ViewController.swift
@@ -11,7 +11,21 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+//        view.backgroundColor = .white
         // Do any additional setup after loading the view.
+        let label = UILabel()
+        label.text = "Hello"
+        label.textColor = .white
+        label.translatesAutoresizingMaskIntoConstraints = false;
+        
+        let xc = NSLayoutConstraint.init(item: label, attribute: .centerX, relatedBy: .equal, toItem: view, attribute: .centerX, multiplier: 1, constant: 0)
+        
+        let yc = NSLayoutConstraint.init(item: label, attribute: .centerY, relatedBy: .equal, toItem: view, attribute: .centerY, multiplier: 1, constant: 0)
+        
+        view.addSubview(label)
+        view.addConstraints([xc,yc])
+        
+        
     }
 
 
