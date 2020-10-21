57483928540a0abef2090ce591eddf7a5ddd823d
Jannik Feuerhahn <jannik@feuer.dev>
2020-10-21 17:48:24 +0800

    setup main layer and routing

--- fishapp/Main/AppDelegate.swift
+++ fishapp/Main/AppDelegate.swift
@@ -11,15 +11,18 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
     
     var window: UIWindow?
+    
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         
-        let w = UIWindow(frame: UIScreen.main.bounds)
-        w.rootViewController = ViewController()
-        self.window = w
-        w.makeKeyAndVisible()
+        let window = UIWindow(frame: UIScreen.main.bounds)
+   
+        let router = Router()
+        
+        window.rootViewController = router.nc
+        self.window = window
+        window.makeKeyAndVisible()
         return true
     }
 }
-
