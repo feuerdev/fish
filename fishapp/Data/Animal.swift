d911db2c4f393ba6bec0ef6b2b4b5e00d9b073e7
Jannik Feuerhahn <jannik@feuer.dev>
2020-10-21 17:49:14 +0800

    added animal model stub

---
+++ fishapp/Data/Animal.swift
@@ -0,0 +1,17 @@
+//
+//  Animal.swift
+//  fishapp
+//
+//  Created by Jannik Feuerhahn on 21.10.20.
+//
+
+import Foundation
+
+enum Danger {
+    case edGreen, edYellow, edRed
+}
+
+struct Animal {
+    var family: String;
+    var category: Danger;
+}
