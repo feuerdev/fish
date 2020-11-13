124a0ba2bad550c3e688fc201b027f71025880dc
Jannik Feuerhahn <jannik@feuer.dev>
2020-11-13 22:38:07 +0800

    DetailViewController

--- fishapp/Presenter/AnimalDetailPresenter.swift
+++ fishapp/Presenter/AnimalDetailPresenter.swift
@@ -20,4 +20,59 @@ class AnimalDetailPresenter {
     init(animal: Animal) {
         self.animal = animal
     }
+    
+    func presentableHierarchy() -> String {
+        var result = ""
+        if animal.kingdom != nil {
+            result.append(">\(animal.kingdom!)")
+        }
+        if animal.phylum != nil {
+            result.append("\n>\(animal.phylum!)")
+        }
+        if animal.subphylum != nil {
+            result.append("\n>\(animal.subphylum!)")
+        }
+        if animal.superclass != nil {
+            result.append("\n>\(animal.superclass!)")
+        }
+        if animal.aclass != nil {
+            result.append("\n>\(animal.aclass!)")
+        }
+        if animal.order != nil {
+            result.append("\n>\(animal.order!)")
+        }
+        if animal.order != nil {
+            result.append("\n>\(animal.order!)")
+        }
+        if animal.family != nil {
+            result.append("\n>\(animal.family!)")
+        }
+        return result
+    }
+    
+    func presentableDangerColor() -> String {
+        return "#ffffff"
+    }
+    
+    func presentableCategory() -> String {
+        switch animal.category {
+        case "NT":
+            return "Not Threatened"
+        default:
+            return ""
+        }
+    }
+    
+    func presentableCategoryColor() -> String {
+        switch animal.category {
+        case "NT":
+            return "#AAAAAA"
+        default:
+            return "#BBBBBB#"
+        }
+    }
+    
+    func presentableSightings() -> String {
+        return "\(animal.records!) Sightings"
+    }
 }
--- fishapp/View/AnimalDetailViewController.swift
+++ fishapp/View/AnimalDetailViewController.swift
@@ -9,7 +9,34 @@ import UIKit
 
 class AnimalDetailViewController: UIViewController {
     
+    @IBOutlet weak var vImageContainer: UIView!
+    @IBOutlet weak var ivPhoto: UIImageView!
+    @IBOutlet weak var lblVernacular: UILabel!
+    @IBOutlet weak var lblScientific: UILabel!
+    @IBOutlet weak var lblTaxonHierarchy: UILabel!
+    @IBOutlet weak var lblSightings: UILabel!
+    @IBOutlet weak var lblCategory: UILabel!
     var presenter: AnimalDetailPresenter?
+    
+    override func viewDidLoad() {
+        
+        guard self.presenter != nil else {
+            return
+        }
+        vImageContainer.layer.cornerRadius = 10
+        ivPhoto.layer.cornerRadius = 10
+        
+        vImageContainer.backgroundColor = UIColor(hexString: presenter!.presentableDangerColor())
+        
+        lblVernacular.text = presenter!.animal.vernacular
+        lblScientific.text = presenter!.animal.family
+        lblTaxonHierarchy.text = presenter!.presentableHierarchy()
+        lblCategory.text = presenter!.presentableCategory()
+        lblCategory.textColor = UIColor(hexString: presenter!.presentableCategoryColor())
+        lblSightings.text = presenter?.presentableSightings()
+        
+    }
+    
 }
 
 extension AnimalDetailViewController: AnimalDetailPresenterDelegate {
