069f96fc17aa3659b93bc8fd5937ebae870489ad
Jannik Feuerhahn <jannik@feuer.dev>
2020-11-25 21:53:12 +0800

    fixed fallthrough error in LoadPhotoService

--- fishapp/Services/LoadPhotoService.swift
+++ fishapp/Services/LoadPhotoService.swift
@@ -9,13 +9,13 @@ import Foundation
 
 class LoadPhotoService {
     
-    static func loadPhoto(id: String, searchParameter: String, completionHandler: @escaping (Result<String, ServiceError>) -> Void) {
+    static func loadPhoto(id: String, searchParameter: String, completionHandler: @escaping (Result<String, Error>) -> Void) {
         DispatchQueue.global(qos: .userInitiated).async {
             //Step 1: Check in UserDefaults for Image
             if let fileName = UserDefaults.standard.string(forKey: id) {
                 //Step 2: Check if family doesn't have photo
                 if fileName == FAMILY_NO_PHOTO {
-                    completionHandler(.failure(.noData))
+                    completionHandler(.failure(ServiceError.noData))
                 } else {
                     completionHandler(.success(fileName))
                 }
@@ -24,25 +24,30 @@ class LoadPhotoService {
                 WikiPhotoURLService.getPhotoUrl(scientificName: searchParameter, completionHandler: { result in
                     switch result {
                     case .failure(let error):
-                        if error as! ServiceError == ServiceError.noData {
-                            UserDefaults.standard.setValue(FAMILY_NO_PHOTO, forKey: id)
-                            completionHandler(.failure(.noData))
-                        }
+                        UserDefaults.standard.setValue(FAMILY_NO_PHOTO, forKey: id)
+                        completionHandler(.failure(error))
                     case .success(let url):
                         //Step 5: Download and save the photo in Documents
-                        if let url = URL(string: url) {
-                            if let data = try? Data(contentsOf: url) {
-                                let documents = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)[0]
-                                let fileUrl = documents.appendingPathComponent(url.lastPathComponent)
-                                do {
-                                    try data.write(to: fileUrl)
-                                    let fileName = fileUrl.lastPathComponent
-                                    UserDefaults.standard.setValue(fileName, forKey: id)
-                                    completionHandler(.success(fileName))
-                                } catch(_) {
-                                    completionHandler(.failure(.couldNotWrite))
-                                }
-                            }
+                        
+                        guard let url = URL(string: url) else {
+                            completionHandler(.failure(ServiceError.noData))
+                            return
+                        }
+                        
+                        guard let data = try? Data(contentsOf: url) else {
+                            completionHandler(.failure(ServiceError.noData))
+                            return
+                        }
+                       
+                        let documents = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)[0]
+                        let fileUrl = documents.appendingPathComponent(url.lastPathComponent)
+                        do {
+                            try data.write(to: fileUrl)
+                            let fileName = fileUrl.lastPathComponent
+                            UserDefaults.standard.setValue(fileName, forKey: id)
+                            completionHandler(.success(fileName))
+                        } catch(_) {
+                            completionHandler(.failure(ServiceError.couldNotWrite))
                         }
                     }
                 })
