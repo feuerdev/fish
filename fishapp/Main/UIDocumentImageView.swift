6b42b2378b2de90ee2defa825919cb447bf8bc8c
Jannik Feuerhahn <jannik@feuer.dev>
2020-11-25 21:51:37 +0800

    Load whole document image now runs in background

--- fishapp/Main/UIDocumentImageView.swift
+++ fishapp/Main/UIDocumentImageView.swift
@@ -12,13 +12,12 @@ class UIDocumentImageView: UIImageView {
     private var filename: String?
     
     func loadImagefromDocuments(filename: String) {
-        if !(self.filename == filename) {
-            self.filename = filename
-            let documents = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)[0]
-            DispatchQueue.global(qos: .userInteractive).async {
+        DispatchQueue.global(qos: .userInitiated).async {
+            if !(self.filename == filename) {
+                self.filename = filename
+                let documents = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)[0]
                 let fileUrl = documents.appendingPathComponent(filename)
-                let img = UIImage(contentsOfFile: fileUrl.path) //TODO: Which thread does this run in?
-                
+                let img = UIImage(contentsOfFile: fileUrl.path)
                 DispatchQueue.main.async {
                     self.image = img
                 }
