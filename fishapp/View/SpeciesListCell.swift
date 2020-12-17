19108f764fab80c261829c6b4d17f7c715f1f508
Jannik Feuerhahn <jannik@feuer.dev>
2020-12-17 17:13:09 +0800

    updated species cell - load images and vernacular - use stackview

--- fishapp/View/SpeciesListCell.swift
+++ fishapp/View/SpeciesListCell.swift
@@ -10,58 +10,71 @@ import Feuerlib
 
 class SpeciesListCell: UITableViewCell {
     
-    @IBOutlet weak var view: UIView!
-    @IBOutlet weak var ivImage: UIDocumentImageView!
+    @IBOutlet weak var ivImage: UIImageView!
     @IBOutlet weak var lblAuthorship: UILabel!
     @IBOutlet weak var lblRank: UILabel!
     @IBOutlet weak var lblRisk: UILabel!
     @IBOutlet weak var lblName: UILabel!
     @IBOutlet weak var lblScientificName: UILabel!
-    @IBOutlet weak var lblRecords: UILabel!
-    @IBOutlet weak var swMarine: UISwitch!
+    
+    var cacheKey: String?
     
     var species: Species? {
         didSet {
-//            ivImage.image =
             if let species = species {
-                if let filename = species.photoFileName {
-                    ivImage.loadImagefromDocuments(filename: filename)
-                }
-                lblName.text = species.vernacular
                 lblScientificName.text = species.getPresentableName()
                 lblAuthorship.text = species.getPresentableAuthorship()
                 lblRank.text = species.taxonRank
                 lblRisk.text = species.getPresentableCategory()
                 lblRisk.textColor = UIColor(hexString: species.getPresentableCategoryColor())
+
+                self.lblName.isSkeletonable = true
+                self.lblName.showAnimatedSkeleton(usingColor: .wetAsphalt)
+
+                self.ivImage.isSkeletonable = true
+                self.ivImage.showAnimatedSkeleton(usingColor: .wetAsphalt)
                 
-                if let records = species.records {
-                    lblRecords.text = String(records)
+                LoadVernacularService.loadVernacular(id: species.taxonId) { result in
+                    switch result {
+                    case .failure(_):
+                        break
+                    case .success(let result):
+                        if(species.taxonId == result.familyId) { //Prevent loading the result after the cell has already been reused
+                            DispatchQueue.main.async {
+                                self.lblName.text = result.vernacular
+                                self.lblName.hideSkeleton()
+                            }
+                        }
+                    }
                 }
-                if let isMarine = species.isMarine {
-                    swMarine.isOn = isMarine
+                
+                if let searchTerm = species.species {
+                    LoadPhotoService.loadPhoto(id: species.taxonId, searchParameter: searchTerm) { result in
+                        switch result {
+                        case .failure(_):
+                            break
+                        case .success(let result):
+                            self.cacheKey = result.url
+                            ImageCache.shared.getImage(from: result.url) { [weak self] result in
+                                guard let self = self else {
+                                    return
+                                }
+                                switch result {
+                                case .success((let cacheKey, let image)):
+                                    if self.cacheKey == cacheKey {
+                                        DispatchQueue.main.async() {
+                                            self.ivImage.image = image
+                                            self.ivImage.hideSkeleton()
+                                        }
+                                    }
+                                case .failure(_):
+                                    break
+                                }
+                            }
+                        }
+                    }
                 }
             }
-            
-//            let documents = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)[0]
-//            if let fileName = animal?.photoFileName {
-//                DispatchQueue.global(qos: .userInitiated).async {
-//                    let fileUrl = documents.appendingPathComponent(fileName)
-//                    let img = UIImage(contentsOfFile: fileUrl.path) //TODO: Which thread does this run in?
-//                    DispatchQueue.main.async {
-//                        self.ivPhoto.image = img
-//                    }
-//                }
-//            }
         }
     }
-
-    override func prepareForReuse() {
-        ivImage.image = nil
-    }
-    
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        self.view.layer.cornerRadius = 15
-    }
-
 }
