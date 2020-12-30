e521bc459375eed671feee9432a941a006fbb9cb
Jannik Feuerhahn <jannik@feuer.dev>
2020-12-30 18:50:40 +0800

    added info view

---
+++ fishapp/View/InfoViewController.swift
@@ -0,0 +1,76 @@
+//
+//  InfoViewController.swift
+//  fishapp
+//
+//  Created by Jannik Feuerhahn on 29.12.20.
+//
+
+import UIKit
+
+class InfoViewController: UIViewController {
+    @IBOutlet weak var tvContent: UITextView!
+    
+    @IBOutlet weak var lblContent: UILabel!
+    @IBOutlet weak var svContent: UIScrollView!
+    @IBOutlet weak var stvContent: UIStackView!
+    override func viewDidLoad() {
+        self.view.backgroundColor = tintColor
+        self.lblContent.textColor = textTintColor
+        
+        self.lblContent.text =
+            """
+            Thank you for using my App!
+            
+            FAQ:
+            
+            Q: Why is there no photo?
+            A: I'm currently getting the photos from Wikipedia. If the Wiki article has no photo, I can not display it in the app.
+
+            Q: What are the three categories?
+            A: The animals are evaluated in three categories:
+                    Red: This animal is dangerous to humans
+                    Yellow: This animal could potentially harm humans, but is usually not aggressive
+                    Green: This animal is harmless to humans
+                        
+            Q: Where do you get the animal data from?
+            A: Animal occurence data comes from OBIS - A Marine Life Database
+
+            Q: Where do you get the vernacular(common) name from?
+            A: I get the english name of the animals from WORMS - Another Marine Life Database
+
+            Q: Where does the categorization come from?
+            A: That was me! I'm not an expert though, so if you want to change a classification please let me know!
+
+            Credit:
+            
+            OBIS - Marine Life Data:
+                OBIS (YEAR) Ocean Biodiversity Information System. Intergovernmental Oceanographic Commission of UNESCO. www.iobis.org.
+
+            WoRMS - Vernacular Names:
+                WoRMS Editorial Board (2020). World Register of Marine Species. Available from https://www.marinespecies.org at VLIZ. Accessed 2020-12-29. doi:10.14284/170
+
+            Map Tiles - OSM
+                © OpenStreetMap contributors
+                www.openstreetmap.org/copyright or www.opendatacommons.org/licenses/odbl.
+                
+
+            Contact Info:
+            jannik@feuer.dev
+            https://feuer.dev
+
+
+            """
+        
+        tvContent.text = lblContent.text
+        tvContent.tintColor = textTintColor
+        tvContent.linkTextAttributes = [.underlineStyle: 1]
+        
+       
+        
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        svContent.contentSize = CGSize(width: svContent.frame.width, height: stvContent.frame.height)
+    }
+    
+}
