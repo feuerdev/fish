aa2bbdef7fe1eb49577090df979b547b9494c0be
Jannik Feuerhahn <jannik@feuer.dev>
2020-12-30 20:52:37 +0800

    updated photoservice to include actually downloading the image It also uses new CachableResult in completionhandler

--- fishapp/Services/LoadPhotoService.swift
+++ fishapp/Services/LoadPhotoService.swift
@@ -5,7 +5,7 @@
 //  Created by Jannik Feuerhahn on 22.11.20.
 //
 
-import Foundation
+import UIKit
 import Feuerlib
 
 /**
@@ -13,34 +13,52 @@ import Feuerlib
  */
 class LoadPhotoService {
     
-    static func loadPhoto(id: Int, searchParameter: String, completionHandler: @escaping (Result<(taxonId:Int, url:String), Error>) -> Void) {
+    static func loadPhoto(id: Int, searchParameter: String, completionHandler: @escaping (CachableResult<Int, UIImage, Error>) -> Void) {
         DispatchQueue.global(qos: .userInitiated).async() {
-            //Step 1: Check in UserDefaults for Image
+            //Step 1: Check in UserDefaults for url
             if let url = UserDefaults.standard.string(forKey: "photo-\(id)") {
                 //Step 2: Check if family doesn't have photo
                 if url == FAMILY_NO_PHOTO {
-                    completionHandler(.failure(ServiceError.noData))
+                    completionHandler(CachableResult(.failure(ServiceError.noData), id))
                 } else {
-                    completionHandler(.success((id, url)))
+                    //Download or load photo from Cache
+                    ImageCache.shared.getImage(from: url) { result in
+                        switch result.result {
+                        case .success(let image):
+                            completionHandler(CachableResult(.success(image), id))
+                        case .failure(let error):
+                            completionHandler(CachableResult(.failure(error), id))
+                            break
+                        }
+                    }
                 }
             } else {
                 //Step 4: Never looked for Photo. Ask Wiki for it.
-                DispatchQueue.global(qos: .userInitiated).async() {
-                        WikiPhotoURLService.getPhotoUrl(scientificName: searchParameter, completionHandler: { result in
-                        switch result {
-                        case .failure(let error):
-                            if let serviceError = error as? ServiceError {
-                                if serviceError == .noData || serviceError == .wrongFiletype {
-                                    UserDefaults.standard.setValue(FAMILY_NO_PHOTO, forKey: "photo-\(id)")
-                                }
+                WikiPhotoURLService.getPhotoUrl(scientificName: searchParameter, completionHandler: { result in
+                    switch result {
+                    case .failure(let error):
+                        if let serviceError = error as? ServiceError {
+                            if serviceError == .noData || serviceError == .wrongFiletype {
+                                //Animal has no photo on wiki or photo with wrong filetype
+                                UserDefaults.standard.setValue(FAMILY_NO_PHOTO, forKey: "photo-\(id)")
                             }
-                            completionHandler(.failure(error))
-                        case .success(let url):
-                            UserDefaults.standard.setValue(url, forKey: "photo-\(id)")
-                            completionHandler(.success((id, url)))
                         }
-                    })
-                }
+                        completionHandler(CachableResult(.failure(error), id))
+                    case .success(let url):
+                        //Found url, now download or get from cache (might be the same photo as already loaded one)
+                        UserDefaults.standard.setValue(url, forKey: "photo-\(id)")
+                        
+                        ImageCache.shared.getImage(from: url) { result in
+                            switch result.result {
+                            case .success(let image):
+                                completionHandler(CachableResult(.success(image), id))
+                            case .failure(let error):
+                                completionHandler(CachableResult(.failure(error), id))
+                                break
+                            }
+                        }
+                    }
+                })
             }
         }
     }
