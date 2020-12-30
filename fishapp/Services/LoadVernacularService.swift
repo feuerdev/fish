4ab6f0bdd1f5a6054df18af4b0e99e3592ebe993
Jannik Feuerhahn <jannik@feuer.dev>
2020-12-30 20:53:09 +0800

    use CachableResult in Vernacular Service

--- fishapp/Services/LoadVernacularService.swift
+++ fishapp/Services/LoadVernacularService.swift
@@ -10,15 +10,15 @@ import Feuerlib
 
 class LoadVernacularService {
     
-    static func loadVernacular(id: Int, completionHandler: @escaping (Result<(familyId:Int, vernacular:String), Error>) -> Void) {
+    static func loadVernacular(id: Int, completionHandler: @escaping (CachableResult<Int, String, Error>) -> Void) {
         DispatchQueue.global(qos: .userInitiated).async {
             //Step 1: Check in UserDefaults for Image
             if let name = UserDefaults.standard.string(forKey: "vernacular-\(id)") {
                 //Step 2: Check if family doesn't have vernacular
                 if name == FAMILY_NO_VERNACULAR {
-                    completionHandler(.failure(ServiceError.noData))
+                    completionHandler(CachableResult(.failure(ServiceError.noData), id))
                 } else {
-                    completionHandler(.success((id, name)))
+                    completionHandler(CachableResult(.success(name), id))
                 }
             } else {
                 //Step 4: Never looked for Vernacular. Ask Worms for it.
@@ -28,10 +28,10 @@ class LoadVernacularService {
                         if error as? ServiceError == ServiceError.noData {
                             UserDefaults.standard.setValue(FAMILY_NO_VERNACULAR, forKey: "vernacular-\(id)")
                         }
-                        completionHandler(.failure(error))
+                        completionHandler(CachableResult(.failure(error), id))
                     case .success(let name):
                         UserDefaults.standard.setValue(name, forKey: "vernacular-\(id)")
-                        completionHandler(.success((id, name)))
+                        completionHandler(CachableResult(.success(name), id))
                     }
                 })
             }
