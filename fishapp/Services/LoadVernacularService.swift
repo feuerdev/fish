4cac3ef31a5ea8f512228f628b5663e17bb79c3a
Jannik Feuerhahn <jannik@feuer.dev>
2021-01-11 18:15:22 +0800

    added exceptions to vernacular service to fix the "Great White Shark" dilemma

--- fishapp/Services/LoadVernacularService.swift
+++ fishapp/Services/LoadVernacularService.swift
@@ -12,7 +12,13 @@ class LoadVernacularService {
     
     static func loadVernacular(id: Int, completionHandler: @escaping (CachableResult<Int, String, Error>) -> Void) {
         DispatchQueue.global(qos: .userInitiated).async {
-            //Step 1: Check in UserDefaults for Image
+            
+            if let exception = exceptionVernacular(id: id) {
+                completionHandler(CachableResult(.success(exception), id))
+                return
+            }
+            
+            //Check in UserDefaults for Image
             if let name = UserDefaults.standard.string(forKey: "vernacular-\(id)") {
                 //Step 2: Check if family doesn't have vernacular
                 if name == FAMILY_NO_VERNACULAR {
@@ -35,6 +41,17 @@ class LoadVernacularService {
                     }
                 })
             }
+        }
+    }
+    
+    static func exceptionVernacular(id:Int) -> String? {
+        switch id {
+        case 105702: //Family of Great White Sharks, people have been complaining when the "Great white Shark was categorized as Yellow"
+            return "Mackerel Sharks"
+        case 105838: //Defaulted to "Anchovie Eater" which is weird
+            return "Great White Shark"
+        default:
+            return nil
         }
     }
 }
