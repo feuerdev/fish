2cd3b5465638cabdb1189b4e0e0461403ebec499
Jannik Feuerhahn <jannik@feuer.dev>
2021-01-12 19:07:24 +0800

    refactored list header cell

--- fishapp/View/FamilyList/FamilyListHeader.swift
+++ fishapp/View/FamilyList/FamilyListHeader.swift
@@ -7,10 +7,6 @@
 
 import UIKit
 
-protocol FamilyListHeaderDelegate: AnyObject {
-    func didCollapse(danger:Danger, collapsed:Bool)
-}
-
 class FamilyListHeader: UICollectionReusableView {
     
     @IBOutlet weak var btnShowTitle: UIButton!
@@ -19,11 +15,38 @@ class FamilyListHeader: UICollectionReusableView {
     @IBOutlet weak var ivImage: UIImageView!
     @IBOutlet weak var lblTitle: UILabel!
     
-    weak var delegate: FamilyListHeaderDelegate?
+    var danger:Danger = .edGreen {
+        didSet {
+            switch danger {
+            case .edGreen:
+                ivImage.image = UIImage(named: "whale30")
+                view.backgroundColor = greenColor
+                lblTitle.text = "Harmless and Wholesome"
+            case .edYellow:
+                ivImage.image = UIImage(named: "warning30")
+                view.backgroundColor = yellowColor
+                lblTitle.text = "Safe, but don't touch or feed"
+            case .edRed:
+                ivImage.image = UIImage(named: "alert30")
+                view.backgroundColor = redColor
+                lblTitle.text = "Dangerous, consult with local experts"
+            }
+        }
+    }
     
-    var danger:Danger = .edGreen
-    var count = 0
-    var collapsed = false
+    var count = 0 {
+        didSet {
+            self.btnShowTitle.setTitle("(\(self.count))", for: .normal)
+        }
+    }
+    
+    var collapsed = false {
+        didSet {
+            self.btnShowImage.transform = !self.collapsed ? .identity : .init(rotationAngle: 180 * CGFloat.pi/180)
+        }
+    }
+    
+    var onCollapse: ((Danger, Bool) -> Void)?
     
     override func awakeFromNib() {
         self.view.layer.cornerRadius = defaultCornerRadius
@@ -35,32 +58,6 @@ class FamilyListHeader: UICollectionReusableView {
     
     @IBAction func showHideSection(_ sender: Any) {
         self.collapsed = !self.collapsed
-        delegate?.didCollapse(danger: self.danger, collapsed: self.collapsed)
-    }
-    
-    func updateButton() {
-        self.btnShowTitle.setTitle("(\(self.count))", for: .normal)
-        self.btnShowImage.transform = !self.collapsed ? .identity : .init(rotationAngle: 180 * CGFloat.pi/180)
-    }
-    
-    func setup(danger:Danger, count:Int, collapsed:Bool) {
-        self.count = count
-        self.danger = danger
-        self.collapsed = collapsed
-        switch danger {
-        case .edGreen:
-            ivImage.image = UIImage(named: "whale30")
-            view.backgroundColor = greenColor
-            lblTitle.text = "Harmless and Wholesome"
-        case .edYellow:
-            ivImage.image = UIImage(named: "warning30")
-            view.backgroundColor = yellowColor
-            lblTitle.text = "Safe, but don't touch or feed"
-        case .edRed:
-            ivImage.image = UIImage(named: "alert30")
-            view.backgroundColor = redColor
-            lblTitle.text = "Dangerous, consult with local experts"
-        }
-        updateButton()
+        self.onCollapse?(danger, self.collapsed)
     }
 }
